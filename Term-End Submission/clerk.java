9eed0d3336fba248b2aa7aabf16f7ca8a589471f
Sufi-san <sufy_456@hotmail.com>
2022-10-22 14:08:59 +0530

    Programs divided into two folders.

---
+++ Term-End Submission/clerk.java
@@ -0,0 +1,14 @@
+//program using super keyword
+class employee {
+ int wt = 8;
+}
+class clerk extends employee {
+ int wt = 10; //work time
+ void display() {
+ System.out.println(super.wt); //will print work time of clerk
+ }
+ public static void main(String args[]) {
+ clerk c = new clerk();
+ c.display();
+ }
+}
